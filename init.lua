396da09e4ec14d25fcc35d0b8af9d5040ae2683d
LoneWolfHT <lonewolf04361@gmail.com>
2019-04-03 15:12:18 -0700

    Fix flashbang bug

--- init.lua
+++ init.lua
@@ -61,6 +61,8 @@ grenades.register_grenade("grenades_basic:regular", {
 
 -- Flashbang Grenade
 
+local flash_huds = {}
+
 grenades.register_grenade("grenades_basic:flashbang", {
 	description = "Flashbang grenade (Blinds all who look at blast)",
 	image = "grenades_flashbang.png",
@@ -69,8 +71,8 @@ grenades.register_grenade("grenades_basic:flashbang", {
 		for _, v in ipairs(minetest.get_objects_inside_radius(pos, 20)) do
 			local hit = minetest.raycast(pos, v:get_pos(), true, true):next()
 
-			if v:is_player() and v:get_hp() > 0 and hit.type == "object" and hit.ref:is_player() and
-			hit.ref:get_player_name() == v:get_player_name() then
+			if v:is_player() and v:get_hp() > 0 and not flash_huds[v:get_player_name()] and hit.type == "object" and
+			hit.ref:is_player() and hit.ref:get_player_name() == v:get_player_name() then
 				local playerdir = vector.round(v:get_look_dir())
 				local grenadedir = vector.round(vector.direction(v:get_pos(), pos))
 				local pname = v:get_player_name()
@@ -82,6 +84,8 @@ grenades.register_grenade("grenades_basic:flashbang", {
 				})
 
 				if math.acos(playerdir.x*grenadedir.x + playerdir.y*grenadedir.y + playerdir.z*grenadedir.z) <= math.pi/4 then
+					flash_huds[pname] = {}
+
 					for i = 0, 5, 1 do
 						local key = v:hud_add({
 							hud_elem_type = "image",
@@ -93,9 +97,19 @@ grenades.register_grenade("grenades_basic:flashbang", {
 							offset = {x = 0, y = 0}
 						})
 
+						flash_huds[pname][i+1] = key
+
 						minetest.after(2 * i, function()
 							if minetest.get_player_by_name(pname) then
 								minetest.get_player_by_name(pname):hud_remove(key)
+
+								if flash_huds[pname] then
+									table.remove(flash_huds[pname], 1)
+								end
+
+								if i == 5 then
+									flash_huds[pname] = nil
+								end
 							end
 						end)
 					end
@@ -105,6 +119,18 @@ grenades.register_grenade("grenades_basic:flashbang", {
 		end
 	end,
 })
+
+minetest.register_on_dieplayer(function(player)
+	local name = player:get_player_name()
+
+	if flash_huds[name] then
+		for _, v in ipairs(flash_huds[name]) do
+			player:hud_remove(v)
+		end
+
+		flash_huds[name] = nil
+	end
+end)
 
 -- Smoke Grenade
 
